3ab21de0c63795c973062a66bfed19289a36532d
Krishna Vamsi Pinninti <krishpinninti789@gmail.com>
2025-02-16 15:26:57 +0530

    dues page search implemented

--- src/app/admin/components/DueDetails.jsx
+++ src/app/admin/components/DueDetails.jsx
@@ -7,12 +7,47 @@ const DueDetails = (dues) => {
   return (
     <div>
       {studentDues.length > 0 ? (
-        studentDues.map((Item, index) => (
-          <div className="text-md font-bold" key={index}>
-            <h1>Year: {Item.year}</h1>
-            <h1>Roll No. :{Item.roll}</h1>
-          </div>
-        ))
+        <div>
+          {studentDues.map((item, index) => (
+            <div key={index}>
+              <h3>Roll: {item.roll}</h3>
+              <h1>Year:{item.year}</h1>
+              <table
+                border="1"
+                cellPadding="10"
+                cellSpacing="0"
+                style={{
+                  width: "100%",
+                  textAlign: "left",
+                  borderCollapse: "collapse",
+                }}
+              >
+                <thead>
+                  <tr>
+                    <th>Type</th>
+                    <th>Amount ($)</th>
+                    <th>Amount paid</th>
+                    <th>Amount pending</th>
+                    <th>Status</th>
+                    <th>Due Date</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {item.dues.map((due, dueIndex) => (
+                    <tr key={dueIndex}>
+                      <td>{due.duetype}</td>
+                      <td>{due.amount}</td>
+                      <td>{due.amount_paid}</td>
+                      <td>{due.amount_pending}</td>
+                      <td>{due.status}</td>
+                      <td>{due.due_date}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          ))}
+        </div>
       ) : (
         <p>No Results..</p>
       )}
